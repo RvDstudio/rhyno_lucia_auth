bec11cae992dffb079999c1a8f0b6995902f5c4b
Reinier <39220977+RvDstudio@users.noreply.github.com>
2024-09-25 00:10:09 +0200

    first commit

---
+++ app/(pages)/sign-in/page.tsx
@@ -0,0 +1,46 @@
+import { SignInForm } from "@/components/SignInForm";
+import { validateRequest } from "@/lib/lucia";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function SignUpPage() {
+  const { user } = await validateRequest();
+
+  if (user) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="w-full h-screen lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid  gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your email below to login to your account
+            </p>
+          </div>
+          <div className="grid gap-6">
+            <SignInForm />
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="/sign-up" className="underline">
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/images/login_bg.png"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
---
+++ app/(pages)/sign-up/page.tsx
@@ -0,0 +1,46 @@
+import { SignUpForm } from "@/components/SignUpForm";
+import { validateRequest } from "@/lib/lucia";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function SignUpPage() {
+  const { user } = await validateRequest();
+
+  if (user) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="w-full h-screen lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Account aanmaken</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your email below to login to your account
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <SignUpForm />
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <Link href="/sign-in" className="underline">
+              Sign in
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/images/login_bg.png"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
